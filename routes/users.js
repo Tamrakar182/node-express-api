950b0273a37249303079707c159c7a32b2ebe476
Manjul <manjultamrakar4@gmail.com>
2022-07-23 23:56:58 +0545

    2nd update

--- routes/users.js
+++ routes/users.js
@@ -15,13 +15,16 @@ const users = [
     }
 ]
 
-router.get('/', (req , res)=>{
-    console.log(users);
-    res.send('Hello');
+router.get('/', (req , res) => {
+    res.send(users);
 })
 
-router.post('/', (req, res)=>{
-    
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push(user);
+
+    res.send(`User with the username ${user.firstName} added to the database!`);
 });
 
 export default router;
