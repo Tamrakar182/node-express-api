edabfb9bbc83b163f0e830ba38584661f7ac5071
Manjul <manjultamrakar4@gmail.com>
2022-07-24 15:23:33 +0545

    finished.

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,18 @@
 import Express from 'express';
-import {v4 as uuidv4} from 'uuid';
+
+import {createUser, getUsers, getUser, deleteUser, updateUser} from '../controllers/users.js';
 
 const router = Express.Router();
 
-let users = [];
 
-router.get('/', (req , res) => {
-    res.send(users);
-})
+router.get('/', getUsers);
 
-router.post('/', (req, res) => {
-    const user = req.body;
+router.post('/', createUser);
 
-    users.push( {... user, id: uuidv4()});
+router.get('/:id', getUser);
 
-    res.send(`User with the username ${user.firstName} added to the database!`);
-});
+router.delete('/:id', deleteUser);
 
-
-// /users/2 => req.params {id : 2}
-
-router.get('/:id', (req,res) => {
-    const { id } = req.params;
-
-    const foundUser = users.find((user) => user.id === id);
-
-    res.send(req.params);
-})
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    //id to delete 123
-
-    //john 123
-    //jane 321
-
-    users = users.filter((user) => user.id !== id);
-
-    res.send(`User with the id ${id} deleted from the database.`);
-
-
-})
+router.patch('/:id', updateUser);
 
 export default router;
