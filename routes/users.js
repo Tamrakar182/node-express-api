b00ea63983d36d4ed6409b1553c1b7f4bd6ba9c3
Manjul <manjultamrakar4@gmail.com>
2022-07-24 15:03:46 +0545

    update v3

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import Express from 'express';
+import {v4 as uuidv4} from 'uuid';
 
 const router = Express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 24  
-    }
-]
+let users = [];
 
 router.get('/', (req , res) => {
     res.send(users);
@@ -22,9 +12,35 @@ router.get('/', (req , res) => {
 router.post('/', (req, res) => {
     const user = req.body;
 
-    users.push(user);
+    users.push( {... user, id: uuidv4()});
 
     res.send(`User with the username ${user.firstName} added to the database!`);
 });
+
+
+// /users/2 => req.params {id : 2}
+
+router.get('/:id', (req,res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(req.params);
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    //id to delete 123
+
+    //john 123
+    //jane 321
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} deleted from the database.`);
+
+
+})
 
 export default router;
